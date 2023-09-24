55699bc604aa5db3ec40dc841d0a15c50de58c34
Alvinchisom <145863920+Alvinchisom@users.noreply.github.com>
2023-09-23 19:28:00 -0700

    Removed comment

--- config/db.py
+++ config/db.py
@@ -4,4 +4,3 @@ from sqlalchemy import create_engine, MetaData
 engine = create_engine("mysql+pymysql://root:Mamaimadeit@localhost:3306/alvin")
 meta = MetaData()
 conn = engine.connect()
-# https://github.com/Alvinchisom/Leo.git
